a322a4a89d6db89a550b3f1099057125329a673d
GemmaTuron <68018621+GemmaTuron@users.noreply.github.com>
2021-07-20 19:05:26 +0200

    scripts for physchem and synthetic accessibility scores for all molecules

--- scripts/1_families.py
+++ scripts/1_families.py
@@ -3,31 +3,105 @@ from __init__ import OUTPUT
 from tqdm import tqdm
 import pandas as pd
 from rdkit import Chem
+import os
 
-# Matching patterns
+#substructures of interest:
+#Triazolo core
+pattern_triazolopyrazine = Chem.MolFromSmarts("*-c1nnc2cncc(-*)n12")
 
-triazolo_patt = Chem.MolFromSmarts("*-c1nnc2cncc(-*)n12")
-def is_triazolo(mol):
-    if mol.HasSubstrMatch(triazolo_patt):
+#Right Hand Side substituent with phenyl ring containing heteroatoms (or not)
+pattern_triazoloheteroaryl = Chem.MolFromSmarts("*-c1cncc2nnc(n12)-[*]:1:[*]:[*]:[*]:[*]:[*]:1")
+
+#RHS substituent with phenyl ring (no heteroatoms)
+pattern_triazolophenyl = Chem.MolFromSmarts("[*]-c1cncc2nnc(-c3ccccc3)n12")
+
+#RHS substituent with phenyl ring (can include heteroatoms) and para substituent on the RHS phenyl
+pattern_triazoloheteroaryl_para = Chem.MolFromSmarts("*-c1cncc2nnc(n12)-[*]:1:[*]:c:[*](-[*]):[*]:[*]:1")
+
+#RHS substituent with phenyl ring (can include heteroatoms) and para substituent on the RHS phenyl
+pattern_triazoloheteroaryl_meta = Chem.MolFromSmarts("*-c1cncc2nnc(n12)-[*]:1:[*]:[*]:[*]:[*](-[*]):[*]:1")
+
+#RHS substituent with phenyl ring (can include heteroatoms) and para substituent on the RHS phenyl
+pattern_triazoloheteroaryl_orto = Chem.MolFromSmarts("*-c1cncc2nnc(n12)-[*]:1:[*]:[*]:[*]:[*]:[*]:1-[*]")
+
+#RHS substituent with a napthalene
+pattern_triazolonaphthalene = Chem.MolFromSmarts("[*]-c1cncc2nnc(-c3ccc4ccccc4c3)n12")
+
+#LHS substituent has an ether
+pattern_pyrazineether = Chem.MolFromSmarts("[*]-[#8]-c1cncc2nnc(-[*])n12")
+
+#LHS substituent has an amide
+pattern_pyrazineamide = Chem.MolFromSmarts("[*]-[#7]-[#6](=O)-c1cncc2nnc(-[*])n12")
+
+def is_triazolopyrazine(mol):
+    if mol.HasSubstructMatch(pattern_triazolopyrazine):
         return 1
     else:
         return 0
 
-def is_heteroaryl(mol):
-    if
+def is_triazoloheteroaryl(mol):
+    if mol.HasSubstructMatch(pattern_triazoloheteroaryl):
+        return 1
+    else:
+        return 0
+
+def is_triazolophenyl(mol):
+    if mol.HasSubstructMatch(pattern_triazolophenyl):
+        return 1
+    else:
+        return 0
+
+def is_triazoloheteroaryl_para(mol):
+    if mol.HasSubstructMatch(pattern_triazoloheteroaryl_para):
+        return 1
+    else:
+        return 0
+
+def is_triazoloheteroaryl_meta(mol):
+    if mol.HasSubstructMatch(pattern_triazoloheteroaryl_meta):
+        return 1
+    else:
+        return 0
+
+def is_triazoloheteroaryl_orto(mol):
+    if mol.HasSubstructMatch(pattern_triazoloheteroaryl_orto):
+        return 1
+    else:
+        return 0
+
+def is_triazolonaphthalene(mol):
+    if mol.HasSubstructMatch(pattern_triazolonaphthalene):
+        return 1
+    else:
+        return 0
+
+def is_pyrazineether(mol):
+    if mol.HasSubstructMatch(pattern_pyrazineether):
+        return 1
+    else:
+        return 0
+
+def is_pyrazineamide(mol):
+    if mol.HasSubstructMatch(pattern_pyrazineamide):
+        return 1
+    else:
+        return 0
 
 
-def is_phenyl(mol):
-
-
-
-df = pd.read_csv(os.path.join(OUTPUT, "data_0.csv"))
+df=pd.read_csv("results/data_0.csv")
 smiles = list(df["Smiles"])
 
 mols = [Chem.MolFromSmiles(smi) for smi in tqdm(smiles)]
 
-df["IsTriazolo"] = [is_triazolo(mol) for mol in mols]
-df[""]
+df["IsTriazoloPyrazine"] = [is_triazolopyrazine(mol) for mol in tqdm(mols)]
+df["TriazoloHeteroaryl"] = [is_triazoloheteroaryl(mol) for mol in tqdm(mols)]
+df["TriazoloPhenyl"] = [is_triazolophenyl(mol) for mol in tqdm(mols)]
+df["TriazoloHeteroarylPara"] = [is_triazoloheteroaryl_para(mol) for mol in tqdm(mols)]
+df["TriazoloHeteroarylMeta"] = [is_triazoloheteroaryl_meta(mol) for mol in tqdm(mols)]
+df["TriazoloHeteroarylOrto"] = [is_triazoloheteroaryl_orto(mol) for mol in tqdm(mols)]
+df["TriazoloNaphthalene"] = [is_triazolonaphthalene(mol) for mol in tqdm(mols)]
+df["PyrazineEther"] = [is_pyrazineether(mol) for mol in tqdm(mols)]
+df["PyrazineAmide"] = [is_pyrazineamide(mol) for mol in tqdm(mols)]
 
-df = df[df["IsTriazolo"] == 1]
+df = df[df["IsTriazoloPyrazine"] == 1]
 df.to_csv(os.path.join(OUTPUT, "data_1.csv"), index=False)
--- scripts/2_physchem.py
+++ scripts/2_physchem.py
@@ -3,25 +3,32 @@ from __init__ import OUTPUT
 from tqdm import tqdm
 import pandas as pd
 from rdkit import Chem
+import os
+
 from rdkit.Chem.Descriptors import qed
 from rdkit.Chem.Descriptors import MolLogP
 from rdkit.Chem.rdMolDescriptors import CalcNumRings
 from rdkit.Chem.Descriptors import MolWt
 
-MAX_MW = 600
+MAX_MW = 550
 MAX_SLOGP = 5
-
+MAX_NUMRINGS = 7
+MIN_QED = 0.35
 
 df = pd.read_csv(os.path.join(OUTPUT, "data_1.csv"))
 
-mols = [Chem.MolFromSmiles(smi) for smi in list(df["Smiles"])]
+mols = [Chem.MolFromSmiles(smi) for smi in tqdm(list(df["Smiles"]))]
 
-df["MolWt"] = [MolWt(mol) for mol in mols]
-df["SLogP"] = [MolLogP(mol) for mol in mols]
-df["NumRings"] = [CalcNumRings(mol) for mol in mols]
-df["Qed"] = [qed(mol) for mol in mols]
+
+df["MolWt"] = [MolWt(mol) for mol in tqdm(mols)]
+df["SLogP"] = [MolLogP(mol) for mol in tqdm(mols)]
+df["NumRings"] = [CalcNumRings(mol) for mol in tqdm(mols)]
+df["Qed"] = [qed(mol) for mol in tqdm(mols)]
+
 
 df = df[df["MolWt"] <= MAX_MW]
 df = df[df["SLogP"] <= MAX_SLOGP]
+df = df[df["NumRings"] <= MAX_NUMRINGS]
+df = df[df["Qed"] >= MIN_QED]
 
 df.to_csv(os.path.join(OUTPUT, "data_2.csv"), index=False)
