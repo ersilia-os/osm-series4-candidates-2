dda273c7e8a4012a1f5bdc75546a8abc6a2d79f5
GemmaTuron <68018621+GemmaTuron@users.noreply.github.com>
2021-07-23 13:51:58 +0200

    readme update

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # osm-series4-candidates-2
-A new round of series 4 candidates, including new low-data generative models and improved predictors
+A new round of series 4 candidates for the [Open Source Malaria Project](https://github.com/opensourcemalaria), including molecules generated from low-data generative models (adapted from the [ETH Modlab](https://github.com/ETHmodlab/virtual_libraries)) and molecules generated in a second round using the [Reinvent 2.0](https://github.com/MolecularAI/Reinvent) generative model with improved activity predictors.
 
+## Molecule generation
+A first batch of molecules were generated in May2021 using the Reinvent 2.0 model. A detailed explanation as well as results analysis of this first round can be found in our github repo [osm-series4-candidates](https://github.com/ersilia-os/osm-series4-candidates). We generated 116728 new series 4 candidates.
+A second batch of molecules (209310) has been generated for the purposes of this analysis using the Reinvent2.0 model that used as activity prediction a Support Vector Classification model (with 0.8 accuracy in test/train sets).
+A third batch of molecules (150365) has been generated using a low-data generative model.
+
+## Selection of best candidates
+All unique final molecules (405766) have undergone a recursive selection process based on physicochemical properties, synthetic accessibility and predicted activity as follows:
 
 ## Run pipeline
 
-For transparency and reproducibility, we provide code to run the full pipeline. Please download and uncompress the following folders:
+For transparency and reproducibility, we provide code to run the full pipeline for candidate selection. Please download and uncompress the following folders:
 
 * [chemprop](https://drive.google.com/file/d/1WDN3NRTC4T98f-6St9YT8wDXO8foZOg5/view?usp=sharing)
 * [grover](https://drive.google.com/file/d/11_zSh1635KcP6GGgiVTozmE96A1N-z-U/view?usp=sharing)
--- app.py
+++ app.py
@@ -5,16 +5,53 @@ import mols2grid
 import streamlit.components.v1 as components
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
+data = os.path.join(ROOT, "scripts", "results", "processed.csv")
 
-filt_step = st.sidebar.selectbox("Filtering step", ["1", "2", "3", "4", "5", "6", "7", "8"])
-data_file = os.path.join(ROOT, "scripts", "results", "data_{0}.csv".format(filt_step))
-df = pd.read_csv(data_file, nrows=10000)
-df = df.sample(1000)
 
+st.set_page_config(layout="wide")# force wide display
+st.title("Molecules for OSM Series 4 by Ersilia Open Source Initiative")
+
+@st.cache
+def load_data():
+    """Loads once then cached for subsequent runs"""
+    df = pd.read_csv(data)
+    return df
+
+df=load_data()
+
+#reorder columns to show activity values first
+df=df[["EosId", "InchiKey", "Smiles", "Activity", "Accessibility", "Similarity", "MolWt", "SLogP", "SAScore", "RAScore", "SybaScore", "IC50Pred", "IC50PredUB", "IC50PredLB", "HighClassifier", "WhalesDist3Act", "Maip", "Qed", "NumRings", "FractionCSP3", "FrHalogen", "HeavyAtom", "Rotatable", "Heteroatoms", "FrAlkylHalide", "TriazoloHeteroaryl", "TriazoloPhenyl", "TriazoloHeteroarylPara", "TriazoloHeteroarylMeta", "TriazoloHeteroarylOrto", "TriazoloNaphthalene", "PyrazineEther", "PyrazineAmide"]]
+df=df.round(3) #round activity to 4 decimals
+
+selection = st.sidebar.selectbox("View Top 20 molecules by", ["Activity", "Accessibility", "Similarity", "None"])
+
+if selection == "Activity":
+    df = df.sort_values('Activity', ascending = False).head(20)
+elif selection == "Accessibility":
+    df = df.sort_values('Accessibility', ascending = False).head(20)
+elif selection == "Similarity":
+    df = df.sort_values('Similarity', ascending = False).head(20)
+else:
+    df = df
+    
 st.write(df)
 
 n_cols = 10
 raw_html = mols2grid.display(df.head(100), smiles_col = "Smiles",
+subset=["img"], tooltip=["Smiles", "EosId"],
+selection=False, n_cols=n_cols)._repr_html_()
+components.html(raw_html, width=None, height=600, scrolling=True)
+
+
+filt_step = st.sidebar.selectbox("Filtering step", ["1", "2", "3", "4", "5", "6", "7", "8"])
+data_file = os.path.join(ROOT, "scripts", "results", "data_{0}.csv".format(filt_step))
+df2 = pd.read_csv(data_file, nrows=10000)
+df2 = df2.sample(1000)
+
+st.write(df2)
+
+n_cols = 10
+raw_html = mols2grid.display(df2.head(100), smiles_col = "Smiles",
 subset=["img"], tooltip=["Smiles"],
 selection=False, n_cols=n_cols)._repr_html_()
 components.html(raw_html, width=None, height=600, scrolling=True)
